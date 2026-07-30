6bf3ed6b992a261464d5c21400d88baac41f05f3
Rupesh Laad <rupesh.laad@gmail.com>
2026-07-30 14:42:03 +0530

    v0.1 Stable Foundation - Web Admin API Docker Prisma Working

--- database/prisma/index.ts
+++ database/prisma/index.ts
@@ -1,1 +1,4 @@
+// Re-export the generated Prisma client so consumers can import from '@cts/database'.
+// The client itself is generated into ./node_modules/@prisma/client by prisma:generate.
 export * from '@prisma/client';
+export { PrismaClient as default } from '@prisma/client';
